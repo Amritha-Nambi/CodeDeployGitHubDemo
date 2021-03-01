1509a4024cc1896d0722f7d1788c31621f8b7bb3
Amritha T N (amtn) <amtn@cisco.com>
2021-03-01 12:17:10 +0530

    Copying a single file

--- sample_modified/app.js
+++ sample_modified/app.js
@@ -9,7 +9,7 @@ var express = require("express"),
     alert = require("alert")
 const mongodbConnection = require("./mongo")
 
-//New Comment Added to understand CodeDeploy
+//Copying only this file
 /*mongoose.set('useNewUrlParser', true); 
 mongoose.set('useFindAndModify', false); 
 mongoose.set('useCreateIndex', true); 
